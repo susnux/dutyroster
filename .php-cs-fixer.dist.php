252399344b589dc365fb25b64f89d4da72231c53
Ferdinand Thiessen <rpm@fthiessen.de>
2022-12-02 17:18:10 +0100

    🧹 [clean] Update basic project files + Update gitignore list
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 // SPDX-FileCopyrightText: Ferdinand Thiessen <rpm@fthiessen.de>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-require_once './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
 
@@ -17,4 +17,7 @@ $config
 	->notPath('src')
 	->notPath('vendor')
 	->in(__DIR__);
+
+$config->setIndent("\t");
+
 return $config;

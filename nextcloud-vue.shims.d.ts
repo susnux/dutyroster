f62e9c124e6276670f8b73c2d5c69c7c47fb7318
Ferdinand Thiessen <rpm@fthiessen.de>
2022-12-04 00:36:54 +0100

    🔨 Port code to Typescript
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

---
+++ nextcloud-vue.shims.d.ts
@@ -0,0 +1,15 @@
+// Allow importing vue files
+declare module '*.vue' {
+	import Vue from 'vue'
+	export default Vue
+}
+
+declare module '@nextcloud/vue';
+
+// Declare global variables from webpack define plugin
+declare const appName: string
+declare const appVersion: string
+
+// Declare webpack
+// eslint-disable-next-line camelcase
+declare let __webpack_public_path__: string

5650f4eaafbee5a5b9dc9b48dddba3e9b696b19c
Ferdinand Thiessen <rpm@fthiessen.de>
2022-12-15 22:25:52 +0100

    🏷️ Add declaration of `vue-material-design-icons`
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

--- nextcloud-vue.shims.d.ts
+++ nextcloud-vue.shims.d.ts
@@ -1,3 +1,8 @@
+/*
+* SPDX-FileCopyrightText: Ferdinand Thiessen <rpm@fthiessen.de>
+* SPDX-License-Identifier: AGPL-3.0-or-later
+*/
+
 // Allow importing vue files
 declare module '*.vue' {
 	import Vue from 'vue'
@@ -6,6 +11,20 @@ declare module '*.vue' {
 
 declare module '@nextcloud/vue';
 declare module 'vue-shortkey';
+
+declare module 'vue-material-design-icons/*.vue' {
+	import type { DefineComponent } from 'vue'
+
+	const IconVue : DefineComponent<
+		{
+			/// `size` defaults to 24
+			size: number,
+			/// `fillColor` defaults to 'currentColor'
+			fillColor: string,
+			title?: string,
+		}>
+	export default IconVue
+}
 
 // Declare global variables from webpack define plugin
 declare const appName: string
